7cafd087a5bf20b6231d3359820c0980f6e90371
LAPTOP-OL0NNPSK\PientaTech <pienta.tech@gmail.com>
2021-01-30 16:32:24 -0500

    working on posts

--- js/hb-helper.js
+++ js/hb-helper.js
@@ -23,6 +23,7 @@ let fetchData = function (name, postdata) {
         })
         .done(function( data ) {
             console.log('success');
+            console.dir(data);
             postdata = JSON.parse(data);
 console.log(postdata);
             postdata.posts.sort(function(a, b) {
