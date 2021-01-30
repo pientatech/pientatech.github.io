51309eae2b5bfe2d1b6310c65c8b9d0d6b564525
LAPTOP-OL0NNPSK\PientaTech <pienta.tech@gmail.com>
2021-01-30 17:37:31 -0500

    working on posts

--- js/hb-helper.js
+++ js/hb-helper.js
@@ -15,19 +15,26 @@ Handlebars.getTemplate = function(name) {
 };
 
 let fetchData = async function (name) {
-    var output;
-    let response = await fetch( 'posts/' + name );
-    if (response.status !== 200) {
-        console.log("Looks like there was a problem. Status Code: " + response.status);
-    } else {
-        const data = await response.json()
-        output = data;
-    }
-    output.posts.sort(function(a, b) {
-        return new Date(a.postdate) - new Date(b.postdate);
-    });
+    // var output;
+    // let response = await fetch( 'posts/' + name );
+    // if (response.status !== 200) {
+    //     console.log("Looks like there was a problem. Status Code: " + response.status);
+    // } else {
+    //     const data = await response.json()
+    //     output = data;
+    // }
+    // output.posts.sort(function(a, b) {
+    //     return new Date(a.postdate) - new Date(b.postdate);
+    // });
 
-    return output;
+    // return output;
+
+
+    let response = await fetch( 'posts/' + name ) 
+    .then((response) => response.json())
+    .then((posts) => {
+        console.dir(posts);
+      });
 };
 
 let fetchPage = function (name) {
