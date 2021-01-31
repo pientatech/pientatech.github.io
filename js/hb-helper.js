5d6470f14de2952860f11bb9f198cecccbc3318d
LAPTOP-OL0NNPSK\PientaTech <pienta.tech@gmail.com>
2021-01-31 10:01:39 -0500

    first two articles

--- js/hb-helper.js
+++ js/hb-helper.js
@@ -14,11 +14,45 @@ Handlebars.getTemplate = function(name) {
   return Handlebars.templates[name];
 };
 
+//this will come from a json file
+var postdata = {};   
 
+$(document).ready(function(){
+    fetchData(`postlog.json`).then(data => {postdata = data; initMainPage();});
+});
+
+var initMainPage = function(){
+    postdata.posts.sort(function(a, b) {
+        return new Date(a.postdate) - new Date(b.postdate);
+    });
+
+    Handlebars.getTemplate('header-template');
+    Handlebars.getTemplate('navigation-template');
+    Handlebars.getTemplate('footer-template');
+    Handlebars.getTemplate('main-template');
+    Handlebars.getTemplate('about-template');
+    Handlebars.getTemplate('post-detail-template');
+
+    //apply footer
+    $('nav').replaceWith(Handlebars.templates['navigation-template']({pages: ['Home','About'], menuname: 'Menu'}));
+    $('header').replaceWith(Handlebars.templates['header-template']({image: 'first.png', heading: 'Cardboard RPG', sub_heading: 'Low-Poly Turn Based RPG'}));
+    $('footer').replaceWith(Handlebars.templates['footer-template']());
+    loadMain();
+};
+
+var lookupPost = function(page){
+    for(var post in postdata.posts){
+        if(postdata.posts[post].page == page){
+            return postdata.posts[post];
+        }
+    }
+
+    return undefined;
+};
 
 let fetchData = async (name) => {
     let response = await fetch('posts/' + name);
-    let data = await response.json()
+    let data = await response.json();
     return data;
 };
 
@@ -34,4 +68,28 @@ let fetchPage = function (name) {
     });
 
     return result;
+};
+
+var loadMain = function () {
+    //grab the top four items from postdata
+    let top4 = {posts: postdata.posts.slice(0, 4) };
+    $('div#main_block').html(Handlebars.templates['main-template'](top4));
+};
+
+var loadNavigation = function (val) {
+    if (val.toLowerCase() == 'home') { 
+        loadMain();
+    } else {
+        $('div#main_block').html(Handlebars.templates[val.toLowerCase() + '-template']());
+    }
+};
+
+var loadPost = function (page) {
+    //get post
+    let post = lookupPost(page);
+
+    //fecth page, add it to template data
+    if(post) {
+        $('div#main_block').html(Handlebars.templates['post-detail-template'](post) + fetchPage(`post-${page}.html`));
+    }
 };
